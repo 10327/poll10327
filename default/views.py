7f861749aa9bc31f3533257b1fb1414618d42f88
unknown <Kuan.Yen>
2020-11-19 11:43:11 +0800

    投票主題新增,修改,刪除

--- default/views.py
+++ default/views.py
@@ -12,13 +12,30 @@ class PollDetail(DetailView):
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
-        options = Options.objects.filter(poll_id=self.kwargs['pk'])
+        options = Option.objects.filter(poll_id=self.kwargs['pk'])
         ctx['option_list'] = options
         return ctx
+
 class PollVote(RedirectView):
     def get_redirect_url(self, * args, ** kwargs):
         option= Option.objects.get(id=self.kwargs['oid'])
         option.count += 1   
         option.save()
-       return '/poll/{}/'. format(option.poll_id)
-       # return'/poll/'+str(option.poll_id)+'/'
+        return '/poll/{}/'.format(option.poll_id)
+        # return'/poll/'+str(option.poll_id)+'/'
+
+class PollCreate(CreateView):
+    model = Poll
+    fields = ['subject','description']
+    success_url = '/poll/'
+
+class PollEdit(UpdateView):
+    model = Poll
+    fields = '__all__'
+    success_url = '/poll/'
+
+class PollDelete(DeleteView):
+    model = Poll
+    success_url = '/poll/'
+
+    
